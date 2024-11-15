cd178829f44ce57e6676fc14f7cc8bfe15f7503f
Felipe Schmitz <schmitzdz14@gmail.com>
2024-11-15 16:49:46 -0300

    Desafio concluido

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
+  console.log('posts: ', posts);
+  console.log('globalData: ', globalData);
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
@@ -26,6 +28,7 @@ export default function Index({ posts, globalData }) {
               <Link
                 as={`/posts/${post.id}`}
                 href={`/posts/${post.id}`}
+                legacyBehavior
               >
                 <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
                   {post.created_ate && (
@@ -61,8 +64,7 @@ export default function Index({ posts, globalData }) {
 
 export async function getServerSideProps() {
   const posts = await getPosts();
-  const globalData = getGlobalData()
-
+  const globalData = getGlobalData();
 
   return { props: { posts, globalData } };
 }
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,23 @@
-import { api } from '../services/api'
+import { data } from 'autoprefixer';
+import { supabase } from '../services/api';
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+  let { data: posts, error } = await supabase
+   .from('posts')
+   .select('*')
 
-    if(data){
-        return data;
-    }
+  if (posts) {
+    return posts;
+  }
 
-    return []
-}
+  return [];
+};
 
 export const getPostBySlug = async (id) => {
-
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+  let { data: posts, error } = await supabase
+    .from('posts')
+    .select('*')
+    // Filters
+    .eq('id', id);
+  return { posts };
+};
